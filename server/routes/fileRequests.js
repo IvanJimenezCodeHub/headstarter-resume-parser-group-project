858fd387be3ca3c9c5dcb681bab3c863f5282c93
Solomon <bangiyevs@gmail.com>
2022-08-13 13:05:50 -0400

    combined endpoints for both filetypes. Combined data sent to client into 1 json. Changed supported files on /upload endpoint. Updated function to upload into only 1 directory

--- server/routes/fileRequests.js
+++ server/routes/fileRequests.js
@@ -4,85 +4,63 @@ const fs = require("fs");
 const XLSX = require("xlsx");
 
 const fileUpload = require("express-fileupload");
-const { response } = require("express");
 
-const textFilesPath = "./uploads/textFiles";
-const spreadsheetPath = "./uploads/spreadsheets";
+const uploadPath = "./uploads/uploadedFiles";
 
 router.use(fileUpload({}));
 
 // GET all files
 router.get("/", (req, res) => {
-  res.json({ users: ["userOne", "userTwo", "userThree", "userFour"] });
-  // This is just dummy data
-});
-
-// GET all xlsx files
-router.get("/excelFiles", (req, res) => {
-  fs.readdir(spreadsheetPath, (err, files) => {
+  fs.readdir(uploadPath, (err, files) => {
     if (err) {
       console.error(err);
     }
-    const candidate = [];
-    files.forEach((fileName) => {
-      const wb = XLSX.readFile(`${spreadsheetPath}/${fileName}`);
-      const sheetName = wb.SheetNames[0];
-      const ws = wb.Sheets[sheetName];
-
-      const data = XLSX.utils.sheet_to_json(ws, { raw: false });
-      candidate.push(...data);
-    });
-    res.json({ candidate });
-  });
-});
-
-// POST
-
-// GET all txt files
-router.get("/plainText", (req, res) => {
-  fs.readdir(textFilesPath, (err, files) => {
-    if (err) {
-      console.error(err);
-    }
-    let candidate = [];
-    let closedCounter = 0;
-
+    const candidates = [];
+    let readCounter = 0;
     files.forEach((fileName, index) => {
-      // send an array of objects to client
-      let obj = { name: "", skills: "", experience: "", contact: "" };
-      const input = fs.createReadStream(`${textFilesPath}/${fileName}`);
-      const rl = require("readline").createInterface({
-        input: input,
-        terminal: false,
-      });
+      if (fileName.includes(".txt")) {
+        let obj = { name: "", skills: "", experience: "", contact: "" };
+        const input = fs.createReadStream(`${uploadPath}/${fileName}`);
+        const rl = require("readline").createInterface({
+          input: input,
+          terminal: false,
+        });
 
-      rl.on("line", (line) => {
-        if (line.includes("Name")) {
-          obj.name = line.slice(line.indexOf(": ") + 2);
-        }
-
-        if (line.includes("Skills")) {
-          obj.skills = line.slice(line.indexOf(": ") + 2);
-        }
-        if (line.includes("Experience")) {
-          obj.experience = line.slice(line.indexOf(": ") + 2);
-        }
-
-        if (line.includes("Contact")) {
-          obj.contact = line.slice(line.indexOf(": ") + 2);
-          candidate.push(obj);
-
-          if (index === files.length - 1) {
-            console.log("file length");
+        rl.on("line", (line) => {
+          if (line.includes("Name")) {
+            obj.name = line.slice(line.indexOf(": ") + 2);
           }
-        }
-      });
-      rl.on("close", () => {
-        closedCounter++;
-        if (closedCounter === files.length - 1) {
-          res.json({ candidate });
-        }
-      });
+
+          if (line.includes("Skills")) {
+            obj.skills = line.slice(line.indexOf(": ") + 2);
+          }
+          if (line.includes("Experience")) {
+            obj.experience = line.slice(line.indexOf(": ") + 2);
+          }
+
+          if (line.includes("Contact")) {
+            obj.contact = line.slice(line.indexOf(": ") + 2);
+            candidates.push(obj);
+          }
+        });
+        rl.on("close", () => {
+          readCounter++;
+          if (readCounter === files.length - 1) {
+            res.json({ candidates });
+          }
+        });
+      } else {
+        const wb = XLSX.readFile(`${uploadPath}/${fileName}`);
+        const sheetName = wb.SheetNames[0];
+        const ws = wb.Sheets[sheetName];
+
+        const data = XLSX.utils.sheet_to_json(ws, { raw: false });
+        candidates.push(...data);
+        readCounter++;
+      }
+      if (readCounter === files.length - 1) {
+        res.json({ candidates });
+      }
     });
   });
 });
@@ -93,42 +71,35 @@ router.post("/upload", (req, res) => {
     return res.status(400).json({ mssg: "no file uploaded" });
   }
   const file = req.files.file; // '.file' comes from the frontend formdata.append
-  // case: file is a .txt
-  if (file.mimetype === "text/plain") {
-    console.log("plain text");
-    file.mv(`${__dirname}/uploads/textFiles/${file.name}`, (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).send(err);
-      }
-      res.json({
-        fileName: file.name,
-        filePath: `/uploads/textFiles/${file.name}`,
-      });
-    });
-  }
-  // case: file is a .numbers or .xlsx. Can also switch this to a general else clause.
-  else if (
-    file.mimetype === "application/octet-stream" ||
+
+  if (
+    file.mimetype === "text/plain" ||
     file.mimetype ===
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
+    file.mimetype === "application/vnd.ms-excel" ||
+    file.mimetype === "application/msexcel" ||
+    file.mimetype === "application/x-msexcel" ||
+    file.mimetype === "application/x-ms-excel" ||
+    file.mimetype === "application/x-excel" ||
+    file.mimetype === "application/x-dos_ms_excel" ||
+    file.mimetype === "application/xls" ||
+    file.mimetype === "application/x-xls" ||
+    file.mimetype === "application/octet-stream"
   ) {
-    file.mv(`${__dirname}/uploads/spreadsheets/${file.name}`, (err) => {
+    console.log("supported file");
+    file.mv(`${__dirname}/uploads/uploadedFiles/${file.name}`, (err) => {
       if (err) {
         console.error(err);
         return res.status(500).send(err);
       }
       res.json({
         fileName: file.name,
-        filePath: `/uploads/spreadsheets/${file.name}`,
+        filePath: `/uploads/uploadedFiles/${file.name}`,
       });
     });
-  }
-  // case: other file mimetypes. NOTE - didn't add support for .xls, xlsb, etc from excel
-  else {
-    console.log("file not supported");
-    res.status(400).json({ error: "file not supported" });
-    // need to send error
+  } else {
+    console.log("unsupported file");
+    res.status(400).json({ error: "filetype not supported" });
   }
 });
 
